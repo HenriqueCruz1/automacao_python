3849743951b9d69de73e9fbbe77fce9f9f41fc24
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-03-16 00:28:47 -0300

    automação de caixas por setor

--- caixas_por_setor.py
+++ caixas_por_setor.py
@@ -15,3 +15,28 @@
 #- % 2
 #- duas listas
 #- append()
+
+setor_par = []
+setor_impar = []
+
+while True:
+    pedidos = int(input('Qual o número do pedido? '))
+
+    if pedidos % 2 == 0:
+        setor_par.append(pedidos)
+    else:
+        setor_impar.append(pedidos)
+    
+    pergunta = input('Deseja cadastrar mais um pedido? [S/N]').upper()
+    while pergunta not in 'SN': 
+        pergunta = input('Digite uma resposta valida [S/N]').upper()
+    if pergunta == 'N':
+        break
+
+setor_impar.sort
+setor_par.sort
+
+print(f'Pedidos do setor par são: {setor_par}')
+print(f'Pedidos do setor impar são: {setor_impar}')
+    
+        
